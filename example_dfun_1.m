d516c69282c1693d3cf46bc4f29e28f20985fd6e
Marcin Konowalczyk <marcin.konowalczyk@some.ox.ac.uk>
2017-07-14 11:54:44 +0100

    wip on dfun plotting

--- example_dfun_1.m
+++ example_dfun_1.m
@@ -1,15 +1,15 @@
 close all; clear; clc;
 load spectra % loads matlab sample data
-
+%{
 %% dfun is working correctly
 tic; NIR_std_1 = std(NIR,1,2); t_base = toc;
 tic; NIR_std_2 = dfun(NIR,@std,2,{1},'verbosity',false); t_dfun = toc;
 fprintf('The result of dfun is correct: %i\n',isequal(NIR_std_1,NIR_std_2));
 fprintf('dfun is ~%.1f times slower than in-build std() function\n',t_dfun./t_base);
-
+%}
 %% Functionality showcase
 xBkg = [36 97 199 329]; % x for background correction
-NIR = dfun(NIR,@(y) y - polyval(polyfit(xBkg,y(xBkg),2),1:length(y)),2);
+NIR = dfun(NIR,@(y) y - polyval(polyfit(xBkg,y(xBkg),2),1:length(y)),2,[],'plot',true);
 dNIR = dfun(NIR,@(x) x - NIR(1,:),2); % Difference spectrum
 rmsNIR = dfun(dNIR,@(x) sqrt(mean(x.^2)),1);
 
